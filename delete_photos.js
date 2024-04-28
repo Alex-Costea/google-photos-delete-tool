86fe8220326cf6960dd01bb85bc5f57ce9dd28d6
Alex Costea <alex@costea.in>
2024-04-28 14:43:06 +0300

    Update delete_photos.js

--- delete_photos.js
+++ delete_photos.js
@@ -1,25 +1,13 @@
-// How many photos to delete?
-// Put a number value, like this
-// const maxImageCount = 5896
-const maxImageCount = "ALL_PHOTOS";
-
 // Selector for Images and buttons
-const ELEMENT_SELECTORS = {
-    checkboxClass: '.ckGgle',
-    languageAgnosticDeleteButton: 'div[data-delete-origin] button',
-    deleteButton: 'button[aria-label="Delete"]',
-    confirmationButton: '#yDmH0d > div.llhEMd.iWO5td > div > div.g3VIld.V639qd.bvQPzd.oEOLpc.Up8vH.J9Nfi.A9Uzve.iWO5td > div.XfpsVe.J9fJmf > button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.nCP5yc.kHssdc.HvOprf'
-}
+const ELEMENT_SELECTORS = { checkboxClass: '.R4HkWb', checkboxClass2: '.ckGgle', languageAgnosticDeleteButton: 'div[data-delete-origin] button', deleteButton: 'button[aria-label="Delete"]', confirmationButton: '#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.V639qd.bvQPzd.oEOLpc.A9Uzve.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW.O4g5Md.iWO5td > div > div.VfPpkd-cnG4Wd.m5OsGf > div > div.VfPpkd-T0kwCb.IdSMxc > button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.nCP5yc.AjY5Oe.LQeN7.kDryjd' }
 
 // Time Configuration (in milliseconds)
 const TIME_CONFIG = {
-    delete_cycle: 10000,
-    press_button_delay: 2000
+    delete_cycle: 3000,
+    press_button_delay: 1000
 };
 
 const MAX_RETRIES = 1000;
-
-let imageCount = 0;
 
 let checkboxes;
 let buttons = {
@@ -28,12 +16,14 @@ let buttons = {
 }
 
 let deleteTask = setInterval(async () => {
-    let attemptCount = 1;
-
     do {
-        checkboxes = document.querySelectorAll(ELEMENT_SELECTORS['checkboxClass']);
+        let checkboxes1 = document.querySelectorAll(ELEMENT_SELECTORS['checkboxClass']);
+        let checkboxes2 = document.querySelectorAll(ELEMENT_SELECTORS['checkboxClass2']);
+        if(checkboxes1.length == 0)
+            checkboxes = new Set([...checkboxes2])
+        else checkboxes = new Set([...checkboxes1])
         await new Promise(r => setTimeout(r, 1000));
-    } while (checkboxes.length <= 0 && attemptCount++ < MAX_RETRIES);
+    } while (checkboxes.length <= 0);
 
 
     if (checkboxes.length <= 0) {
@@ -43,8 +33,6 @@ let deleteTask = setInterval(async () => {
         return;
     }
 
-    attemptCount = 1;
-    imageCount += checkboxes.length;
 
     checkboxes.forEach((checkbox) => { checkbox.click() });
     console.log("[INFO] Deleting", checkboxes.length, "images");
@@ -61,15 +49,6 @@ let deleteTask = setInterval(async () => {
         setTimeout(() => {
             buttons.confirmation_button = document.querySelector(ELEMENT_SELECTORS['confirmationButton']);
             buttons.confirmation_button.click();
-
-            console.log(`[INFO] ${imageCount}/${maxImageCount} Deleted`);
-            if (maxImageCount !== "ALL_PHOTOS" && imageCount >= parseInt(maxImageCount)) {
-                console.log(`${imageCount} photos deleted as requested`);
-                clearInterval(deleteTask);
-                console.log("[SUCCESS] Tool exited.");
-                return;
-            }
-
         }, TIME_CONFIG['press_button_delay']);
     }, TIME_CONFIG['press_button_delay']);
 }, TIME_CONFIG['delete_cycle']);
